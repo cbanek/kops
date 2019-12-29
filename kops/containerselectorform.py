f68c47b90b8e10f37ba0b15fceb2968a9670faa2
Christine Banek <cbanek@gmail.com>
2019-12-29 01:01:28 -0700

    Keep selected items selected over changes
    Keep the originally selected items selected when the list of pods
    and namespaces is refreshed.  If someone selects a namespace,
    and a new pod shows up under it, then select that one automatically.

--- kops/containerselectorform.py
+++ kops/containerselectorform.py
@@ -20,30 +20,46 @@ class ContainerSelectorForm(npyscreen.FormBaseNew):
         if self.pod_update_id == id:
             return None
 
+        # Build the tree, and collate namespaces.
         namespaces = {}
         r = npyscreen.TreeData(selectable=False, ignore_root=True)
 
         for p in pods:
             namespace = namespaces.get(p.metadata.namespace, None)
-
             if namespace is None:
                 namespace = r.new_child(content=p.metadata.namespace)
                 namespaces[p.metadata.namespace] = namespace
+            namespace.key = p.metadata.namespace
 
             pod = namespace.new_child(content=p.metadata.name)
             if len(p.spec.containers) == 1:
                 pod.data = p
+                pod.key = '.'.join([namespace.key, p.metadata.name, p.spec.containers[0].name])
             else:
+                pod.key = '.'.join([namespace.key, p.metadata.name])
                 for container in p.spec.containers:
                     c = pod.new_child(content=container.name)
+                    c.key = '.'.join([namespace.key, p.metadata.name, container.name])
                     p_copy = copy.deepcopy(p)
                     p_copy.spec.containers = [container]
                     c.data = p_copy
-                    
 
+        # Here we copy over what was selected before to the new tree.
+        # We use the key property for this, which allows us to figure out
+        # which items are set, and set them in the new tree.  If a node is
+        # selected, select all the items below it using the walk_tree.
+        # So if a namespace is selected and gets a new pod, select that.
+        selected_keys = [i.key for i in self.tree.get_selected_objects()]
+
+        for i in r.walk_tree():
+            if i.key in selected_keys:
+                for j in i.walk_tree(ignore_root=False):
+                    j.selected = True
+
+        # Update and redraw.
         self.tree.values = r
-        self.pod_update_id = id
         self.tree.display()
+        self.pod_update_id = id
 
     def onBack(self, *args, **keywords):
         self.selected_containers = []
